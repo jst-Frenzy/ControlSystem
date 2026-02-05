5598590d1a6a80575ea1f740032d65fe04ee8b43
jst-Frenzy <Lamiging@gmail.com>
2026-02-05 23:41:30 +0300

    add docker-compose, possibility run app

--- AuthService/internal/dataBase/postgresDB.go
+++ AuthService/internal/dataBase/postgresDB.go
@@ -1,6 +1,10 @@
 package dataBase
 
 import (
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,5 +18,25 @@ func InitPostgres() {
 	var err error
 	if PostgresDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}); err != nil {
 		logrus.WithError(err).Fatal("Failed to connect to database")
+	}
+	runMigrations(os.Getenv("MIGRATE_DB_DSN"))
+}
+
+func runMigrations(dsn string) {
+	m, errCreate := migrate.New("file:///app/migrations", dsn)
+	if errCreate != nil {
+		logrus.WithError(errCreate).Info("cant initialize migrations")
+		return
+	}
+	defer func() {
+		if _, err := m.Close(); err != nil {
+			logrus.WithError(errCreate).Info("cant close migrations")
+		}
+	}()
+
+	err := m.Up()
+
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		logrus.WithError(err).Info("Migration failed")
 	}
 }
--- AuthService/migrations/20260201151829_v1.up.sql
+++ AuthService/migrations/20260201151829_v1.up.sql
@@ -10,7 +10,7 @@ create table users(
 
 create table refresh_tokens(
     id serial not null,
-    user_id integer not null references users(id) on delete cascade
+    user_id integer not null references users(id) on delete cascade,
     token_hash varchar(255) not null,
     created_at timestamp default now(),
     updated_at timestamp default now(),
